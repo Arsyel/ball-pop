e34b36456d20898e54212b24c870a318aa95b553
Arsyel <ahmad.hakim@tokopedia.com>
2021-09-22 13:48:08 +0700

    add options param

--- src/ts/modules/gameobjects/MatterSprite.ts
+++ src/ts/modules/gameobjects/MatterSprite.ts
@@ -5,8 +5,8 @@ export class MatterSprite {
 	private _gameObject: Phaser.Physics.Matter.Sprite;
 	private _transform: Transform;
 
-	constructor (private _scene: Phaser.Scene, x: number, y: number, texture: string, frame = 0) {
-		this._gameObject = _scene.matter.add.sprite(x, y, texture, frame);
+	constructor (private _scene: Phaser.Scene, x: number, y: number, texture: string, frame = 0, options?: Phaser.Types.Physics.Matter.MatterBodyConfig) {
+		this._gameObject = _scene.matter.add.sprite(x, y, texture, frame, options);
 		this._transform = new Transform(_scene, this._gameObject);
 	}
 
